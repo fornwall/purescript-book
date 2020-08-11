503f63aed1b42fab9e7f86a682f236b4d5205284
Fredrik Fornwall <fredrik@fornwall.net>
2020-08-11 14:18:39 +0200

    Chapter 3, exercise 3

--- exercises/chapter3/test/Main.purs
+++ exercises/chapter3/test/Main.purs
@@ -66,7 +66,6 @@ main =
       test "Lookup missing" do
         Assert.equal Nothing
           $ findEntryByStreet "456 Nothing St." book
-{-  Move this block comment starting point to enable more tests
     suite "Exercise - isInBook" do
       test "Check existing" do
         Assert.equal true
@@ -74,6 +73,7 @@ main =
       test "Check missing" do
         Assert.equal false
           $ isInBook "unknown" "person" book
+{-  Move this block comment starting point to enable more tests
     test "Exercise - removeDuplicates" do
       Assert.equal book
         $ removeDuplicates bookWithDuplicate
--- exercises/chapter3/test/MySolutions.purs
+++ exercises/chapter3/test/MySolutions.purs
@@ -2,9 +2,9 @@ module Test.MySolutions where
 
 import Prelude
 
-import Data.List (List(..), filter, head)
+import Data.List (filter, head, null)
 import Data.AddressBook (AddressBook, Entry)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe)
 
 
 findEntryByStreet :: String -> AddressBook -> Maybe Entry
@@ -13,3 +13,8 @@ findEntryByStreet streetName = head <<< filter filterEntry
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.address.street == streetName
 
+isInBook :: String -> String -> AddressBook -> Boolean
+isInBook firstName lastName = not null <<< filter filterEntry
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
